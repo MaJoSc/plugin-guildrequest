83a2b5cb3ae628ab1311590cb3358bc7408a430c
badtwin <badtwin@eqdkp-plus.eu>
2009-05-07 19:38:57 +0000

    typos in varnames are bad ;)

--- language/german/lang_main.php
+++ language/german/lang_main.php
@@ -131,5 +131,5 @@ $lang['gr_ad_replyadress']                = 'Dies ist eine automatisch erstellte
 $lang['gr_sendermail']                    = 'Absender:';
 $lang['gr_ad_popup_activated']					  = 'Popup bei inaktiven Bewerbungen anzeigen? (Wird nur benötigt, wenn der Mailversand nicht funktioniert)';
 $lang['gr_ad_notactivated_popup']					= 'Bewerbung noch nicht aktiviert!';
-$lang['gr_poll_voted_yed']                = 'Zwischenstand ';
+$lang['gr_poll_voted_yet']                = 'Zwischenstand ';
 ?>
