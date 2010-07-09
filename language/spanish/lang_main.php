f2f2437634f481ab214d2ca99d0525300be545ca
corgan <corgan@eqdkp-plus.eu>
2010-07-09 15:58:20 +0000

    french & spanish translation

--- language/spanish/lang_main.php
+++ language/spanish/lang_main.php
@@ -1,145 +1,120 @@
-ï»¿<?PHP
-/********************************************\
-* Guildrequest Plugin for EQdkp plus         *
-* ------------------------------------------ *
-* Project Start: 01/2009                     *
-* Author: BadTwin                            *
-* Copyright: Andreas (BadTwin) Schrottenbaum *
-* Link: http://eqdkp-plus.com                *
-* Version: 0.0.2                             *
-\********************************************/
+<?php
 
-global $eqdkp;
-$lang['guildrequest']                     = 'Solicitudes de Hermandad';
-$lang['request'] 						  = 'PeticiÃ³n';
-$lang['gr_short_desc']                    = 'Plugin de Reclutamiento';
-$lang['gr_long_desc']                     = 'Un plugin para el reclutamiento';
+if (!defined('EQDKP_INC')) {
+	die('You cannot access this file directly.');
+}
 
-// Userdaten fÃ¼r den Gastuser
-$lang['gr_user_aspirant']                 = 'Aspirante';
-$lang['gr_user_email']                    = 'Perfil del invitado para el plugin de Solicitudes de Hermandad';
+//Language: Spanish	
+//Created by EQdkp Plus Translation Tool on  2010-07-09 15:10
+//File: plugin_guildrequest
+//Source-Language: english
 
-//Editor
-$lang['editor_language']	= 'es';
-
-// Admin Menu
-$lang['gr_manage']												= 'administrar';
-$lang['gr_view']                          = 'Ver Peticiones';
-$lang['gr_write']                         = 'Escribir peticiÃ³n';
-
-// Bewerbung erstellen
-$lang['gr_write_headline']                = 'Escribir peticiÃ³n';
-$lang['gr_write_incorrect_mail']          = 'Has introducido una direcciÃ³n de correo invÃ¡lida';
-$lang['gr_write_allfields']               = 'Â¡Hay que rellenar todos los campos!';
-$lang['gr_write_sendrequest']             = 'enviar peticiÃ³n';
-$lang['gr_write_reset']                   = 'resetear';
-$lang['gr_write_error']                   = 'Error';
-$lang['gl_write_succ']                    = 'Correo enviado';
-$lang['gr_mailsent']                      = 'Por favor, confirma tu PeticiÃ³n haciendo click en el enlace que encontrarÃ¡s en el correo.';
-$lang['gr_mail_topic']                    = 'Confirma tu peticiÃ³n en '.preg_replace("'", "\'", $eqdkp->config['guildtag']);
-$lang['gr_mail_text1']                    = 'Por favor, confirma tu peticiÃ³n haciendo click en el siguiente enlace:';
-$lang['gr_mail_text2']                    = 'Â¡Esperamos que tengas un buen dÃ­a! El LÃ­der de la Hermandad.';
-$lang['gr_username_f']                    = 'usuario:';
-$lang['gr_email_f']                       = 'correo:';
-$lang['gr_password_f']                    = 'contraseÃ±a:';
-$lang['gr_text_f']                        = 'texto:';
-$lang['gr_settings']                      = 'Ajustes';
-$lang['gr_user_double']                   = 'Un usuario con el mismo nombre ha enviado ya una peticiÃ³n. Por favor, elige otro nombre.';
-$lang['gr_welcome_text']                  = 'Gracias por tu interÃ©s en '.preg_replace("'", "\'", $eqdkp->config['guildtag']).'. Por favor, escribe la peticiÃ³n abajo:';
-
-// BestÃ¤tigung
-$lang['gr_activate_succ']                 = 'Â¡Tu peticiÃ³n se ha enviado!';
-
-// Login
-$lang['gr_login_headline']                = 'PeticiÃ³n - Inicio de SesiÃ³n';
-$lang['gr_login_succ']                    = 'Inicio de sesiÃ³n correcto';
-$lang['gr_login_not_activated']           = 'No has confirmado el correo de registro.';
-$lang['gr_login_wrong']                   = 'Nombre de usuario o contraseÃ±a incorrecto.';
-$lang['gr_login_empty']                   = 'Â¡Por favor, rellena todos los campos!';
-$lang['gr_login_submit']                  = 'login';
-$lang['gr_login_reset']                   = 'resetear';
-$lang['gr_showrequest_headline']          = 'PeticiÃ³n: ';
-$lang['gr_answer_f']                      = 'Respuesta:';
-$lang['gr_closed_headline']               = 'La peticiÃ³n se ha cerrado.';
-
-// Member-Ansicht
-$lang['gr_vr_not_voted']                  = 'Â¡No has votado aun!';
-$lang['gr_vr_voted']                      = 'Â¡Tu voto ha sido aceptado!';
-$lang['gr_goback']                        = 'atrÃ¡s';
-$lang['gr_poll_headline']                 = 'Â¿DeberÃ­a invitarse al candidato a la hermandad';
-$lang['gr_poll_yes']                      = 'Si';
-$lang['gr_poll_no']                       = 'No';
-  // Admin-Ansicht
-  $lang['gr_poll_ad_opened']              = 'Abierta';
-  $lang['gr_poll_ad_closed']              = 'Cerrada';
-  $lang['gr_poll_ad_save']                = 'Guardada';
-  $lang['gr_ad_adminonly']                = 'peticiones cerradas - sÃ³lo los administradores pueden ver:';
-  $lang['gr_ad_delete']                   = 'borrar';
-  $lang['gr_ad_activate']                 = 'activar';
-  $lang['gr_not_activated']               = 'Peticiones no actiadas:';
-  $lang['gr_no_requests']                 = 'No hay peticiones existentes.';
-  // Info-Boxen
-  $lang['gr_vr_ad_opened_f']              = 'Abierta';
-  $lang['gr_vr_ad_opened']                = 'La peticiÃ³n ha sido abierta';
-  $lang['gr_vr_ad_closed_f']              = 'Cerrada';
-  $lang['gr_vr_ad_closed']                = 'La peticiÃ³n ha sido cerrada';
-  $lang['gr_vr_ad_activated_f']           = 'Activada';
-  $lang['gr_vr_ad_activated']             = 'La peticiÃ³n ha sido activada';
-  $lang['gr_vr_ad_deleted_f']             = 'Borrada';
-  $lang['gr_vr_ad_deleted']               = 'La peticiÃ³n ha sido borrada';
-
-
-// Administrationsbereich
-$lang['gr_ad_config_headline']            = 'Peticiones - Ajustes';
-$lang['gr_ad_poll_activated']             = 'Encuestas activadas';
-$lang['gr_ad_headline_f']                 = 'texto de bienvenida:';
-$lang['gr_ad_mail1_f']                    = 'primera parte del correo de registro:';
-$lang['gr_ad_mail2_f']                    = 'segunda parte del correo de registro:';
-$lang['gr_ad_update_succ']                = 'Â¡los ajustes se han guardado!';
-$lang['gr_ad_update_succ_hl']             = 'Â¡Ã‰xito!';
-
-// Portal Module
-$lang['gr_pm_one_not_voted']              = 'Hay una peticiÃ³n esperando tu voto.';
-$lang['gr_pm_not_voted_1']                = 'Â¡Hay ';
-$lang['gr_pm_not_voted_2']                = ' peticiones esperando tus votos!';
-
-$lang['gr_pu_new_query']                  = 'Nueva peticiÃ³n: ';
-
-
-
-
-$lang['gr_form_manage']                   = 'Editar Formulario';
-$lang['gr_ad_form_singletext']            = 'Texto Ãºnico';
-$lang['gr_ad_form_textfield']             = 'Campo de texto';
-$lang['gr_ad_form_dropdown']              = 'Desplegable';
-$lang['gr_ad_fieldname_f']                = 'Nombre del campo';
-$lang['gr_ad_fieldtype_f']                = 'Tipo de campo';
-$lang['gr_ad_requiredfield_f']            = 'Requerido';
-$lang['gr_ad_editdropdown']               = 'Editar Opciones';
-$lang['gr_ad_editoptions']                = 'Editar Opciones';
-$lang['gr_ad_succ_head']                  = 'Cambios guardados';
-$lang['gr_ad_succ_text']                  = 'Los cambios se guardaron conrrectamente';
-$lang['gr_ad_err_dropdown']               = 'No has elegido un nombre de campo';
-$lang['gr_ad_succ_del']                   = 'Campo borrado correctamente';
-$lang['gr_ad_sort_f']                     = 'OrdenaciÃ³n';
-$lang['gr_ad_preview_f']                  = 'Vista previa';
-$lang['gr_vr_view']                       = 'Ver Solicitud';
-$lang['gr_comment']                       = 'Escribir comentarios';
-$lang['gr_ad_closingmail']                = 'Informar aspirante';
-$lang['gr_ad_closingtext']                = 'Tu solicitud ha sido cerrada. Nuestros usuarios han votado de la siguiente manera:';
-$lang['gr_ad_replyadress']                = 'Este es un correo generado automÃ¡ticamente. Por favor, envÃ­a tu respuesta a: ';
-$lang['gr_sendermail']                    = 'De:';
-$lang['gr_ad_popup_activated']		  			= 'Â¿Mostrar aviso cuando existan solicitudes inactivas? (sÃ³lo es necesario si el envÃ­o de correos no funciona)';
-$lang['gr_ad_notactivated_popup']					= 'Â¡Solicitud no activada!';
-$lang['gr_poll_voted_yet']                = 'resultado intermedio ';
-$lang['gr_vote']                          = 'Voto';
-$lang['gr_ad_form_headline']              = 'Cabecera';
-$lang['gr_ad_form_spaceline']             = 'lÃ­nea vacÃ­a';
-$lang['gr_ad_sm_submit']									= 'enviar';
-
-$lang['gr_mailnotsent']										= 'Error al enviar correo de activaciÃ³n. Por favor, contacta con el administrador.';
-$lang['gr_write_error']										= 'Â¡Error!';
-$lang['gr_closingmail_hl']								= 'Solicitud cerrada';
-$lang['gr_closingmail_submit']						= 'Enviar';
-$lang['gr_wrongcaptcha']									= 'Â¡Has introducido un cÃ³digo de confirmaciÃ³n incorrecto!';
+$alang = array( 
+"guildrequest" => "Solicitudes de Hermandad",
+"request" => "Petición",
+"gr_short_desc" => "Plugin de Reclutamiento",
+"gr_long_desc" => "Un plugin para el reclutamiento",
+"gr_user_aspirant" => "Aspirante",
+"gr_user_email" => "Perfil del invitado para el plugin de Solicitudes de Hermandad",
+"editor_language" => "es",
+"gr_manage" => "administrar",
+"gr_view" => "Ver Peticiones",
+"gr_write" => "Escribir petición",
+"gr_write_headline" => "Escribir petición",
+"gr_write_incorrect_mail" => "Has introducido una dirección de correo inválida",
+"gr_write_allfields" => "¡Hay que rellenar todos los campos!",
+"gr_write_sendrequest" => "enviar petición",
+"gr_write_reset" => "resetear",
+"gr_write_error" => "¡Error!",
+"gl_write_succ" => "Correo enviado",
+"gr_mailsent" => "Por favor, confirma tu Petición haciendo click en el enlace que encontrarás en el correo.",
+"gr_mail_topic" => "Confirma tu petición en ",
+"gr_mail_text1" => "Por favor, confirma tu petición haciendo click en el siguiente enlace:",
+"gr_mail_text2" => "¡Esperamos que tengas un buen día! El Líder de la Hermandad.",
+"gr_username_f" => "usuario:",
+"gr_email_f" => "correo:",
+"gr_password_f" => "contraseña:",
+"gr_text_f" => "texto:",
+"gr_settings" => "Ajustes",
+"gr_user_double" => "Un usuario con el mismo nombre ha enviado ya una petición. Por favor, elige otro nombre.",
+"gr_welcome_text" => "Gracias por tu interés en . Por favor, escribe la petición abajo:",
+"gr_activate_succ" => "¡Tu petición se ha enviado!",
+"gr_login_headline" => "Petición - Inicio de Sesión",
+"gr_login_succ" => "Inicio de sesión correcto",
+"gr_login_not_activated" => "No has confirmado el correo de registro.",
+"gr_login_wrong" => "Nombre de usuario o contraseña incorrecto.",
+"gr_login_empty" => "¡Por favor, rellena todos los campos!",
+"gr_login_submit" => "login",
+"gr_login_reset" => "resetear",
+"gr_showrequest_headline" => "Petición: ",
+"gr_answer_f" => "Respuesta:",
+"gr_closed_headline" => "La petición se ha cerrado.",
+"gr_vr_not_voted" => "¡No has votado aun!",
+"gr_vr_voted" => "¡Tu voto ha sido aceptado!",
+"gr_goback" => "atrás",
+"gr_poll_headline" => "¿Debería invitarse al candidato a la hermandad",
+"gr_poll_yes" => "Si",
+"gr_poll_no" => "No",
+"gr_poll_ad_opened" => "Abierta",
+"gr_poll_ad_closed" => "Cerrada",
+"gr_poll_ad_save" => "Guardada",
+"gr_ad_adminonly" => "peticiones cerradas - sólo los administradores pueden ver:",
+"gr_ad_delete" => "borrar",
+"gr_ad_activate" => "activar",
+"gr_not_activated" => "Peticiones no actiadas:",
+"gr_no_requests" => "No hay peticiones existentes.",
+"gr_vr_ad_opened_f" => "Abierta",
+"gr_vr_ad_opened" => "La petición ha sido abierta",
+"gr_vr_ad_closed_f" => "Cerrada",
+"gr_vr_ad_closed" => "La petición ha sido cerrada",
+"gr_vr_ad_activated_f" => "Activada",
+"gr_vr_ad_activated" => "La petición ha sido activada",
+"gr_vr_ad_deleted_f" => "Borrada",
+"gr_vr_ad_deleted" => "La petición ha sido borrada",
+"gr_ad_config_headline" => "Peticiones - Ajustes",
+"gr_ad_poll_activated" => "Encuestas activadas",
+"gr_ad_headline_f" => "texto de bienvenida:",
+"gr_ad_mail1_f" => "primera parte del correo de registro:",
+"gr_ad_mail2_f" => "segunda parte del correo de registro:",
+"gr_ad_update_succ" => "¡los ajustes se han guardado!",
+"gr_ad_update_succ_hl" => "¡Éxito!",
+"gr_pm_one_not_voted" => "Hay una petición esperando tu voto.",
+"gr_pm_not_voted_1" => "¡Hay ",
+"gr_pm_not_voted_2" => " peticiones esperando tus votos!",
+"gr_pu_new_query" => "Nueva petición: ",
+"gr_form_manage" => "Editar Formulario",
+"gr_ad_form_singletext" => "Texto único",
+"gr_ad_form_textfield" => "Campo de texto",
+"gr_ad_form_dropdown" => "Desplegable",
+"gr_ad_fieldname_f" => "Nombre del campo",
+"gr_ad_fieldtype_f" => "Tipo de campo",
+"gr_ad_requiredfield_f" => "Requerido",
+"gr_ad_editdropdown" => "Editar Opciones",
+"gr_ad_editoptions" => "Editar Opciones",
+"gr_ad_succ_head" => "Cambios guardados",
+"gr_ad_succ_text" => "Los cambios se guardaron conrrectamente",
+"gr_ad_err_dropdown" => "No has elegido un nombre de campo",
+"gr_ad_succ_del" => "Campo borrado correctamente",
+"gr_ad_sort_f" => "Ordenación",
+"gr_ad_preview_f" => "Vista previa",
+"gr_vr_view" => "Ver Solicitud",
+"gr_comment" => "Escribir comentarios",
+"gr_ad_closingmail" => "Informar aspirante",
+"gr_ad_closingtext" => "Tu solicitud ha sido cerrada. Nuestros usuarios han votado de la siguiente manera:",
+"gr_ad_replyadress" => "Este es un correo generado automáticamente. Por favor, envía tu respuesta a: ",
+"gr_sendermail" => "De:",
+"gr_ad_popup_activated" => "¿Mostrar aviso cuando existan solicitudes inactivas? (sólo es necesario si el envío de correos no funciona)",
+"gr_ad_notactivated_popup" => "¡Solicitud no activada!",
+"gr_poll_voted_yet" => "resultado intermedio ",
+"gr_vote" => "Voto",
+"gr_ad_form_headline" => "Cabecera",
+"gr_ad_form_spaceline" => "línea vacía",
+"gr_ad_sm_submit" => "enviar",
+"gr_mailnotsent" => "Error al enviar correo de activación. Por favor, contacta con el administrador.",
+"gr_closingmail_hl" => "Solicitud cerrada",
+"gr_closingmail_submit" => "Enviar",
+"gr_wrongcaptcha" => "¡Has introducido un código de confirmación incorrecto!",
+ );
+$lang = (is_array($lang))? $lang : array();
+$lang = array_merge($lang, $alang);
 ?>
