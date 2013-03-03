4e1f9cc4512db630601fb815c036dd0f40bfb3c7
godmod <godmod@eqdkp-plus.eu>
2013-03-03 09:41:45 +0000

    Fixed language errors in guildrequest

--- guildrequest_plugin_class.php
+++ guildrequest_plugin_class.php
@@ -38,7 +38,7 @@ class guildrequest extends plugin_generic
     return array_merge(parent::$shortcuts, $shortcuts);
   }
 
-  public $version    = '0.1.6';
+  public $version    = '0.1.7';
   public $build      = '';
   public $copyright  = 'GodMod';
   public $vstatus    = 'Alpha';
