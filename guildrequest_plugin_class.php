f54c965b42d8a08243e52f1c648d08137196411e
GodMod <godmod23@web.de>
2018-05-21 17:55:02 +0200

    Implemented user deletion hook

--- guildrequest_plugin_class.php
+++ guildrequest_plugin_class.php
@@ -28,7 +28,7 @@ if (!defined('EQDKP_INC')){
   +--------------------------------------------------------------------------*/
 class guildrequest extends plugin_generic {
 
-	public $version				= '1.1.4';
+	public $version				= '1.1.5';
 	public $build				= '';
 	public $copyright			= 'GodMod';
 
