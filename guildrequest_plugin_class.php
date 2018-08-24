82ec970397643547fcb5ffcb7f364f3c9a055d87
GodMod <GodMod@users.noreply.github.com>
2018-08-24 16:59:11 +0200

    Bump version

--- guildrequest_plugin_class.php
+++ guildrequest_plugin_class.php
@@ -28,7 +28,7 @@ if (!defined('EQDKP_INC')){
   +--------------------------------------------------------------------------*/
 class guildrequest extends plugin_generic {
 
-	public $version				= '1.1.6';
+	public $version				= '1.1.7';
 	public $build				= '';
 	public $copyright			= 'GodMod';
 
